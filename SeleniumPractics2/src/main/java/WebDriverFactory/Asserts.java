30a471014857430405a1ceeca6376c724724a09f
Denis Martynov <idenismartynov@yandex.ru>
2023-11-03 17:55:03 +0300

    Переработал второй Селениум код с параметризацией, отрефакторил и перезаливаю его, удалил старый репозиторий Selenium2

---
+++ SeleniumPractics2/src/main/java/WebDriverFactory/Asserts.java
@@ -0,0 +1,20 @@
+package WebDriverFactory;
+
+// Класс для ожидаемых значений при сравнении через Assert
+public class Asserts {
+
+
+    //===============================================================================================================
+     /*
+        БЛОК АССЕРТОВ
+                                                                                                                                                              */
+    public static final String EXPECTED_TEXT_1_QUESTION = "Сколько это стоит? И как оплатить?";
+    public static final String EXPECTED_TEXT_2_QUESTION = "Хочу сразу несколько самокатов! Так можно?";
+    public static final String EXPECTED_TEXT_3_QUESTION = "Как рассчитывается время аренды?";
+    public static final String EXPECTED_TEXT_4_QUESTION = "Можно ли заказать самокат прямо на сегодня?";
+    public static final String EXPECTED_TEXT_5_QUESTION = "Можно ли продлить заказ или вернуть самокат раньше?";
+    public static final String EXPECTED_TEXT_6_QUESTION = "Вы привозите зарядку вместе с самокатом?";
+    public static final String EXPECTED_TEXT_7_QUESTION = "Можно ли отменить заказ?";
+    public static final String EXPECTED_TEXT_8_QUESTION = "Я жизу за МКАДом, привезёте?";
+
+}
---
+++ SeleniumPractics2/src/main/java/WebDriverFactory/WaitSettings.java
@@ -0,0 +1,6 @@
+package WebDriverFactory;
+
+public class WaitSettings {
+
+    public static final int WAIT_2_SECOND = 2;
+}
