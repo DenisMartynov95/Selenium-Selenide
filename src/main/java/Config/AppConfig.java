b004cc089ae0c3f8825778cc5030e78f6731df60
Denis Mart <idenismartynov@yandex.ru>
2023-12-22 14:02:04 +0300

    _-

--- src/main/java/Config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package Config;
-
-public class AppConfig {
-
-    public static final String URL = "https://qa-mesto.praktikum-services.ru/signup";
-    public static final String URL2 = "https://qa-mesto.praktikum-services.ru/signin";
-
-}
-
